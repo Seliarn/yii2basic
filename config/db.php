583b8309a37c8ebd5efc366e734696591fdbd8a9
Seliarn <popov.ser92@gmail.com>
2017-04-24 22:35:39 +0300

    first commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => '66336333',
+    'charset' => 'utf8',
+];
